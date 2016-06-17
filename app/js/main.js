664e9bb183abf83f091b0bf2552665a7a8b25e47
Daniel Geymayer <dag0310@gmail.com>
2016-06-17 23:16:18 +0200

    Use 'var' instead of 'let' again because Safari does not support it (yet)

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,29 @@
-/*jslint browser, es6, this*/
+/*jslint browser, this*/
 /*global window, $, Morris*/
-let Main = (function () {
+var Main = (function () {
     'use strict';
 
     $.ajaxSetup({
         cache: false
     });
 
-    let publicMethods = {};
+    var publicMethods = {};
 
     publicMethods.init = function (CONFIG) {
-        let loadingIconClassName = 'loading-icon';
-        let $WINDOW = $(window);
-        let $CHART = $('#chart');
-        let $TABLE = $('#table');
-        let $FORM = $('form');
+        var loadingIconClassName = 'loading-icon';
+        var $WINDOW = $(window);
+        var $CHART = $('#chart');
+        var $TABLE = $('#table');
+        var $FORM = $('form');
 
-        let TEXT = {
+        var TEXT = {
             timeStamp: 'Timestamp',
             sys: 'SYS [mmHg]',
             dia: 'DIA [mmHg]',
             pulse: 'Pulse [1/min]'
         };
 
-        let CHART = Morris.Line({
+        var CHART = Morris.Line({
             element: 'chart',
             data: [],
             xkey: CONFIG.keys.dateTime,
@@ -55,9 +55,9 @@ let Main = (function () {
         function setTableData(logData) {
             $TABLE.empty();
 
-            let table = $('<table>');
+            var table = $('<table>');
 
-            let firstRow = $('<tr>');
+            var firstRow = $('<tr>');
             firstRow.append($('<th>', {text: TEXT.timeStamp}));
             firstRow.append($('<th>', {text: TEXT.sys}));
             firstRow.append($('<th>', {text: TEXT.dia}));
@@ -65,7 +65,7 @@ let Main = (function () {
             table.append(firstRow);
 
             logData.forEach(function (value) {
-                let newRow = $('<tr>');
+                var newRow = $('<tr>');
                 newRow.append($('<td>', {text: value[CONFIG.keys.dateTime]}));
                 newRow.append($('<td>', {text: value[CONFIG.keys.sys]}));
                 newRow.append($('<td>', {text: value[CONFIG.keys.dia]}));
@@ -93,11 +93,11 @@ let Main = (function () {
         }
 
         $FORM.on('submit', function () {
-            let now = new Date();
-            let localDateTime = new Date(now.getTime() - now.getTimezoneOffset() * 60 * 1000);
-            let isoDateTimeStringNoTimeZone = localDateTime.toISOString().substr(0, 19);
-            let dateTimeStringArray = isoDateTimeStringNoTimeZone.split('T');
-            let dateTimeString = dateTimeStringArray[0] + ' ' + dateTimeStringArray[1];
+            var now = new Date();
+            var localDateTime = new Date(now.getTime() - now.getTimezoneOffset() * 60 * 1000);
+            var isoDateTimeStringNoTimeZone = localDateTime.toISOString().substr(0, 19);
+            var dateTimeStringArray = isoDateTimeStringNoTimeZone.split('T');
+            var dateTimeString = dateTimeStringArray[0] + ' ' + dateTimeStringArray[1];
 
             $(this).find('input[name="' + CONFIG.keys.dateTime + '"]').val(dateTimeString);
         });
