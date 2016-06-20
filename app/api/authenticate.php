18818efbca5a804821e2b0ee35b6846366827b0f
Daniel Geymayer <dag0310@gmail.com>
2016-06-20 22:13:46 +0200

    Put two lines into one line

--- app/api/authenticate.php
+++ app/api/authenticate.php
@@ -8,8 +8,7 @@ function unauthorized() {
     die ('Unauthorized');
 }
 
-list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) =
-  explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
 
 if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
     unauthorized();
