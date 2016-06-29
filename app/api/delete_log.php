ab4804e78c9a31fab3a0fc8d5e194ac69d9049f1
Daniel Geymayer <dag0310@gmail.com>
2016-06-29 23:22:38 +0200

    Refactoring: Use constants instead of hardcoded strings

--- app/api/delete_log.php
+++ app/api/delete_log.php
@@ -3,7 +3,7 @@
 require_once('../config.php');
 require_once('authenticate.php');
 
-if (! isset($_POST['dateTime']) || ! is_string($_POST['dateTime'])) {
+if (! isset($_POST[KEY_DATETIME]) || ! is_string($_POST[KEY_DATETIME])) {
     header('HTTP/1.1 400 Bad Request');
     exit;
 }
@@ -13,8 +13,8 @@ $log_data = file_get_contents(STORAGE_FILEPATH);
 $lines = explode("\n", $log_data);
 $new_lines = array();
 foreach ($lines as $line) {
-    if (strpos($line, $_POST['dateTime']) !== FALSE) {
-         continue;
+    if (strpos($line, $_POST[KEY_DATETIME]) !== FALSE) {
+        continue;
     }
     $new_lines[] = $line;
 }
